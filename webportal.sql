19e10b5864fe148f35af63ed57e02759e37b2c5f
HusnainAjmal <45016898+HusnainAjmal@users.noreply.github.com>
2018-12-01 15:03:04 -0800

    db
    uPDATED db

--- webportal.sql
+++ webportal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2018 at 10:22 PM
+-- Generation Time: Dec 02, 2018 at 12:01 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
@@ -63,6 +63,57 @@ INSERT INTO `member` (`ID`, `Name`, `Lname`, `Email`, `ContactNumber`, `Utype`, 
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `productdetails`
+--
+
+CREATE TABLE `productdetails` (
+  `pid` int(12) NOT NULL,
+  `sid` int(12) NOT NULL,
+  `name` varchar(50) DEFAULT 'Default Product',
+  `issued` int(12) DEFAULT '0',
+  `available` int(12) DEFAULT '0',
+  `NetWeight` int(12) DEFAULT '0',
+  `NetValue` int(12) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `productdetails`
+--
+
+INSERT INTO `productdetails` (`pid`, `sid`, `name`, `issued`, `available`, `NetWeight`, `NetValue`) VALUES
+(1, 12, 'Liquid Nitrogen', 20, 0, 10, 1000),
+(1, 13, 'Liquid Nitrogen', 10, 2, 5, 25),
+(1, 8, 'Sulphuric Acid', 10, 0, 10, 120),
+(1, 11, 'Sodium carbonate Pentahidrate', 10, 2, 10, 200);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `ProductID` int(12) NOT NULL,
+  `ProductName` varchar(50) DEFAULT 'Default Product',
+  `QuantityProduced` int(12) DEFAULT '0',
+  `QuantityIssued` int(12) DEFAULT '0',
+  `QuantityAvailable` int(12) DEFAULT '0',
+  `PriceperKG` float DEFAULT '0',
+  `ProductDate` datetime DEFAULT CURRENT_TIMESTAMP,
+  `comments` varchar(500) DEFAULT 'No Comments'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ProductID`, `ProductName`, `QuantityProduced`, `QuantityIssued`, `QuantityAvailable`, `PriceperKG`, `ProductDate`, `comments`) VALUES
+(1, 'Head&Shoulder', 100, 0, 100, 13.45, '2018-12-13 00:00:00', 'No Comments'),
+(2, 'AlfhaBeta', 100, 0, 100, 1.5, '2018-12-05 00:00:00', 'No Comments');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `stocks`
 --
 
@@ -84,9 +135,12 @@ CREATE TABLE `stocks` (
 --
 
 INSERT INTO `stocks` (`StockID`, `SupplierID`, `StockName`, `QuantityPurchased`, `QuantityIssued`, `QuantityAvailable`, `PriceperKG`, `TotalPrice`, `StockDate`, `comments`) VALUES
-(3, 13, 'Alumunium', 12, 0, NULL, 100, 1200, '2018-11-09 00:00:00', 'Wahaj Group of industries has been providing High quality Aluminium to our company. We highly value their service as their customer                             '),
-(4, 51, 'Sulphuric Acid', 123, 0, NULL, 321, 39483, '2018-11-07 00:00:00', 'hbtgvcfdxy6gtfr      '),
-(7, 45, 'Liquid Nitrogen', 65, 0, NULL, 100, 6500, '2018-11-10 00:00:00', 'jkvjmncbvnbvb      ');
+(8, 35, 'Sulphuric Acid', 10, 10, 0, 12, 120, '2018-11-08 00:00:00', 'y6gtfrdyhgtfredy6gtfred'),
+(9, 13, 'Sulphuric Acid', 12, -4, 4, 10, 120, '2018-11-17 00:00:00', 'vrfcdbtvrcfex'),
+(10, 45, 'Liquid Nitrogen', 10, -8, 8, 10, 100, '2018-11-24 00:00:00', 'hbtgvcrfedx'),
+(11, 51, 'Sodium carbonate Pentahidrate', 12, -1, 1, 20, 240, '2018-11-01 00:00:00', 'No Comments'),
+(12, 36, 'Liquid Nitrogen', 20, 20, 0, 100, 2000, '2018-11-01 00:00:00', 'No Comments'),
+(13, 13, 'Liquid Nitrogen', 12, -2, 2, 5, 60, '2018-11-14 00:00:00', 'No Comments');
 
 -- --------------------------------------------------------
 
@@ -141,6 +195,12 @@ ALTER TABLE `member`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ProductID`);
+
+--
 -- Indexes for table `stocks`
 --
 ALTER TABLE `stocks`
@@ -161,12 +221,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1111111112;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ProductID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `stocks`
 --
 ALTER TABLE `stocks`
-  MODIFY `StockID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `StockID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `user`
 --
